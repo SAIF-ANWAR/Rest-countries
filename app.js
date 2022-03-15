ea8d2b35cc1c071c1a8a9669f01c54b7791e8629
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-03-15 22:21:15 +0600

    push

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayCountries(data))
 }
 loadCountries()
+
+const displayCountries = (countries) => {
+
+    const allCountriesHTML = countries.map(country => getCountryHTML(country));
+    console.log(country);
+    const container = document.getElementById('country');
+    container.innerHTML = allCountriesHTML.join(' ');
+
+
+}
+const getCountryHTML = country => {
+    return `
+    <div  class="countries"> 
+        <h2>${country.name.common} </h2> 
+        <img src="${country.flags.png}">
+    </div>
+    `
+}
